7a06ca6aa32cdc627ab17ad778d2e4f66a4353c1
Jassayy <jasgandhi2004@gmail.com>
2025-12-07 21:37:59 +0530

    aws amplify deployment ready

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone", //required for aws
+  // output: "standalone", //required for aws
 };
 
 export default nextConfig;
