1b9eba2f470c90cfb5021a343e0ea76525a0497e
Jassayy <jasgandhi2004@gmail.com>
2025-12-08 12:13:31 +0530

    redis and dump fetch updated

--- app/api/dumps/route.ts
+++ app/api/dumps/route.ts
@@ -1,3 +1,7 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
 //this file is made for fetching all the dumps the user has saved
 
 import { NextRequest, NextResponse } from "next/server";
